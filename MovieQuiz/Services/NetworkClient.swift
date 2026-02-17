e71048b1dc7354e6e8cbe8326ab9a13a6be74aa3
el.mkh <el.mkh@inbox.ru>
2026-02-17 23:16:43 +0300

    Исправление

--- MovieQuiz/Services/NetworkClient.swift
+++ MovieQuiz/Services/NetworkClient.swift
@@ -17,19 +17,22 @@ struct NetworkClient {
         let request = URLRequest(url: url)
         
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            let result: Result<Data, Error>
+            
             if let error = error {
-                handler(.failure(error))
-                return
+                result = .failure(error)
+            } else if let response = response as? HTTPURLResponse,
+        response.statusCode < 200 || response.statusCode >= 300 {
+                result = .failure(NetworkError.codeError)
+            } else if let data = data {
+                result = .success(data)
+            } else {
+                result = .failure(NetworkError.codeError)
             }
             
-            if let response = response as? HTTPURLResponse,
-               response.statusCode < 200 || response.statusCode >= 300{
-                handler(.failure(NetworkError.codeError))
-                return
+            DispatchQueue.main.async {
+                handler(result)
             }
-            
-            guard let data = data else { return }
-            handler(.success(data))
             
             }
         task.resume()
